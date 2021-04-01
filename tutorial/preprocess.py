14d63caed78d4b611667db7dad81a835a2e0b61b
vandedok <vandedok@ispras.ru>
2021-04-01 20:27:34 +0300

    reformated the code

--- tutorial/preprocess.py
+++ tutorial/preprocess.py
@@ -10,107 +10,114 @@ from torch.utils.data import Dataset, DataLoader
 
 from utils import stratified_split
 
+
 class AlbMNIST(MNIST):
     """Pytorch MNIST dataset adapted to use albumentaions lib """
 
     def __init__(self, *args, alb_transforms=None, **kwargs):
         super().__init__(*args, **kwargs)
-        self.alb_transforms = alb_transforms    
-             
+        self.alb_transforms = alb_transforms
+
     def set_transofrms(self, transforms):
         self.alb_transforms = transforms
-        
+
     def __getitem__(self, idx):
         temp_transform = self.transform
         self.transform = None
         image, label = super().__getitem__(idx)
         self.transform = temp_transform
         image_np = np.array(image)
-        
+
         if self.alb_transforms is not None:
-            
-            image_alb = self.alb_transforms(image=image_np)['image']
+
+            image_alb = self.alb_transforms(image=image_np)["image"]
 
             return {"original": image_np, "augmented": image_alb, "label": label}
-        
+
         else:
             return {"original": image_np, "label": label}
-        
-        
+
+
 def compose_array_from_dataloader(dataloader, key="original"):
-    """Creates a numpy array from a pytorch dataloader. 
-           
+    """Creates a numpy array from a pytorch dataloader.
+
     Parameters
     ----------
     dataloader : torch.utils.data.dataloader.DataLoader
-        the initial dataloader providing data to be composed as numpy array 
+        the initial dataloader providing data to be composed as numpy array
     key : str
-        specifies what is going to be composed. 
+        specifies what is going to be composed.
         "original" stands for original MNIST dataset,
         "augmented" stands for the aufgmented images,
         "label" stands for annotations
-        
+
     Returns
     -------
     output_array: numpy.array
         The output array
         If "key" param is "original" or "augmented" the array's shape is (N, H, W) where N = len(dataloader), H, W are images width and height
-        If "key: param is "label" the output shape is (N,) where N = len(dataloader)      
+        If "key: param is "label" the output shape is (N,) where N = len(dataloader)
     """
 
-    
     sample = dataloader.dataset[0][key]
-    
+
     if key == "label":
         dtype = np.int
         output_shape = [len(dataloader.dataset)]
     else:
         dtype = np.float32
         output_shape = [len(dataloader.dataset)] + list(sample.shape)
-        
+
     output_array = np.zeros(output_shape, dtype=dtype)
     output_array.setflags(write=True)
     global_batch_size = dataloader.batch_size
-    
-         
 
     with tqdm(total=len(dataloader)) as pbar:
         for idx, batch in enumerate(dataloader):
             array_to_add = batch[key].numpy()
             batch_size = array_to_add.shape[0]
-            output_array[global_batch_size*idx : global_batch_size*idx+batch_size] = array_to_add
+            output_array[
+                global_batch_size * idx : global_batch_size * idx + batch_size
+            ] = array_to_add
             pbar.update(1)
-            
+
     return output_array
 
 
 def save_dataset_as_numpy(dataloader, file_path, key="original", message=""):
-    """ Saves the data provided by dataloader as numpy array
-    
+    """Saves the data provided by dataloader as numpy array
+
     Parameters
     ----------
     dataloader : torch.utils.data.dataloader.DataLoader
-        the initial dataloader providing data to be saved as numpy array 
+        the initial dataloader providing data to be saved as numpy array
     file_path : str
         path to output file
     key : str
-        specifies what is going to be composed. 
+        specifies what is going to be composed.
         "original" stands for original MNIST dataset,
         "augmented" stands for the aufgmented images,
         "label" stands for annotations
     message : str
         message to print
     """
-    
+
     print(message)
     array_to_save = compose_array_from_dataloader(dataloader, key=key)
     with open(file_path, "wb") as file:
-        np.save(file, array_to_save) 
-        
-        
-def create_MNIST_np_files_with_preprocessed_augs(alb_transforms, target_dir = ".", aug_number=10, fraction_to_take = 1., batch_size=4, num_workers=1):
+        np.save(file, array_to_save)
+
+
+def create_MNIST_np_files_with_preprocessed_augs(
+    alb_transforms,
+    target_dir=".",
+    aug_number=10,
+    fraction_to_take=1.0,
+    batch_size=4,
+    num_workers=1,
+):
     """creates several np files with original MNIST dataset and its augmented versions
-    
+
     Parameters
     ----------
     alb_transforms : albumentations.core.composition.Compose
@@ -129,107 +136,115 @@ def create_MNIST_np_files_with_preprocessed_augs(alb_transforms, target_dir = ".
     batch_size : int
         size of batch to feed into internal dataloader. May affec the performance
     """
-    
+
     if not os.path.exists(target_dir):
-        print("%s is not found, creating...", end = " ")
+        print("%s is not found, creating...", end=" ")
         os.mkdir(target_dir)
 
-        
     dataset_full = AlbMNIST(
-        root = os.path.join(target_dir, "MNIST"),
-        download=True, 
-        alb_transforms=None
+        root=os.path.join(target_dir, "MNIST"), download=True, alb_transforms=None
     )
-    
-    
-    if fraction_to_take < 1.:
+
+    if fraction_to_take < 1.0:
         print("Creating split...", end=" ")
-        dataset_alb, _  = stratified_split(dataset_full, train_size=fraction_to_take)
+        dataset_alb, _ = stratified_split(dataset_full, train_size=fraction_to_take)
         print("Done!")
     else:
         dataset_alb = dataset_full
-    
-    dataloader = DataLoader(dataset_alb, batch_size=batch_size, shuffle=False, num_workers=num_workers)
-    
+
+    dataloader = DataLoader(
+        dataset_alb, batch_size=batch_size, shuffle=False, num_workers=num_workers
+    )
+
     dataset_full.set_transofrms(None)
     original_mnist_path = os.path.join(target_dir, "mnist_original.np")
     labels_path = os.path.join(target_dir, "mnist_labels.np")
     dataset_full.set_transofrms(alb_transforms)
-    
-    save_dataset_as_numpy(dataloader, original_mnist_path, key="original", message="Saving original dataset as np file")
-    save_dataset_as_numpy(dataloader, labels_path, key="label", message="Saving labels as np file")
+
+    save_dataset_as_numpy(
+        dataloader,
+        original_mnist_path,
+        key="original",
+        message="Saving original dataset as np file",
+    )
+    save_dataset_as_numpy(
+        dataloader, labels_path, key="label", message="Saving labels as np file"
+    )
 
     for i in range(aug_number):
-        aug_mnist_path = os.path.join(target_dir, "mnist_aug_" + str(i) +".np")
-        save_dataset_as_numpy(dataloader, aug_mnist_path, key="augmented", message="Creating augmented dataset #%i"%i)
+        aug_mnist_path = os.path.join(target_dir, "mnist_aug_" + str(i) + ".np")
+        save_dataset_as_numpy(
+            dataloader,
+            aug_mnist_path,
+            key="augmented",
+            message="Creating augmented dataset #%i" % i,
+        )
 
-    
-    
-def create_MNIST_arrays(alb_transforms=None, aug_number=1, target_dir=".",  batch_size=256, num_workers=1):
-    """ Create numpy arrays containing the MNIST dataset, the labels and the augmented images 
-            
-        Parameters
-        ----------
-        alb_transforms : albumentations.core.composition.Compose
-            a composition of Albumentations transforms
-        aug_number : int
-            number of augmentations to make
-        target_dir : str
-            dir to store the dataset
-        batch_size : int
-            size of batch used in augmentation
-        num_workers : int
-            number of CPU threads to use in augmentations
-            
-        Returns
-        -------
-        originals_array : numpy.ndarray
-            array with original MNIST images
-        labels_array : numpy.ndarray
-            array with MNIST labels
-        aug_arrays : list of numpy.ndarrays
-            list wih augmented versions of MNIST images
-        
-        """
+
+def create_MNIST_arrays(
+    alb_transforms=None, aug_number=1, target_dir=".", batch_size=256, num_workers=1
+):
+    """Create numpy arrays containing the MNIST dataset, the labels and the augmented images
+
+    Parameters
+    ----------
+    alb_transforms : albumentations.core.composition.Compose
+        a composition of Albumentations transforms
+    aug_number : int
+        number of augmentations to make
+    target_dir : str
+        dir to store the dataset
+    batch_size : int
+        size of batch used in augmentation
+    num_workers : int
+        number of CPU threads to use in augmentations
+
+    Returns
+    -------
+    originals_array : numpy.ndarray
+        array with original MNIST images
+    labels_array : numpy.ndarray
+        array with MNIST labels
+    aug_arrays : list of numpy.ndarrays
+        list wih augmented versions of MNIST images
+
+    """
 
     dataset_alb = AlbMNIST(os.path.join(target_dir, "MNIST"), download=True)
-    dataloader = DataLoader(dataset_alb, batch_size=len(dataset_alb), shuffle=False, num_workers=num_workers)
+    dataloader = DataLoader(
+        dataset_alb, batch_size=len(dataset_alb), shuffle=False, num_workers=num_workers
+    )
 
-    print("Fetching original dataset...", end =" ")
-    originals_array = next(iter(dataloader))['original'].numpy()
-    labels_array = next(iter(dataloader))['label'].numpy()
+    print("Fetching original dataset...", end=" ")
+    originals_array = next(iter(dataloader))["original"].numpy()
+    labels_array = next(iter(dataloader))["label"].numpy()
     print("Done!")
 
     dataset_alb.set_transofrms(alb_transforms)
     aug_arrays = []
     dataloader = DataLoader(
-        dataset_alb, 
-        batch_size=batch_size, 
-        shuffle=False, 
-        num_workers=num_workers
+        dataset_alb, batch_size=batch_size, shuffle=False, num_workers=num_workers
     )
 
     for aug_idx in range(aug_number):
-        print("Making aug #%i"%aug_idx)
-        aug_arrays.append(compose_array_from_dataloader(dataloader, key = "augmented"))
-
+        print("Making aug #%i" % aug_idx)
+        aug_arrays.append(compose_array_from_dataloader(dataloader, key="augmented"))
 
     return originals_array, labels_array, aug_arrays
 
 
 def manually_download_MNIST(DATASET_DIR):
     """Downloads MNIST from github attachments
-    
+
     This solution is made for the situations when the MNIST from the official cite is unavailiable
-    
+
     Paramters
     ---------
     DATASET_DIR : str
         path to dir where MNIST is going to be sotred.
         The same path shoul be in DATASET_DIR initialization
     """
-    
-    
+
     output_path = os.path.join(DATASET_DIR, "MNIST.zip")
     if not os.path.exists(DATASET_DIR):
         os.mkdir(DATASET_DIR)
@@ -237,6 +252,6 @@ def manually_download_MNIST(DATASET_DIR):
     print("Downloading MNIST...", end=" ")
     urllib.request.urlretrieve(url, output_path)
     print("Done!")
-    
-    with zipfile.ZipFile(output_path, 'r') as zip_ref:
+
+    with zipfile.ZipFile(output_path, "r") as zip_ref:
         zip_ref.extractall(DATASET_DIR)
--- tutorial/utils.py
+++ tutorial/utils.py
@@ -5,11 +5,11 @@ from torch import nn
 from sklearn.model_selection import train_test_split
 from IPython.display import clear_output
 from torch.utils.data import Dataset
-        
-        
+
+
 def visualize_augmentations(dataset, samples=5, initial_idx=0):
     """Visualises several samples from original dataset and threir augmented counterpars
-    
+
     Parameters
     ----------
     dataset : utils.NumpyAugDataset
@@ -18,36 +18,37 @@ def visualize_augmentations(dataset, samples=5, initial_idx=0):
         number of samples to show
     initial_idx : int
         initial index. The images shown have the following indices:
-        initial_idx, initial_idx+ 1, ..., initial_idx + samples 
+        initial_idx, initial_idx+ 1, ..., initial_idx + samples
     """
 
-    figure, ax = plt.subplots(nrows=2, ncols=samples, figsize=(12, 6))   
+    figure, ax = plt.subplots(nrows=2, ncols=samples, figsize=(12, 6))
 
     for i in range(samples):
-        images = dataset[initial_idx+i]
-        ax[0, i].imshow(images["original"].transpose([1,2,0]), cmap='gray')
-        ax[1, i].imshow(images["aug"].transpose([1,2,0]), cmap='gray')
+        images = dataset[initial_idx + i]
+        ax[0, i].imshow(images["original"].transpose([1, 2, 0]), cmap="gray")
+        ax[1, i].imshow(images["aug"].transpose([1, 2, 0]), cmap="gray")
 
     plt.tight_layout()
-    plt.show()  
-    
-    
+    plt.show()
+
+
 def weight_init(model):
     """Initialises the model weights"""
-    
+
     if isinstance(model, nn.Conv2d):
-        nn.init.xavier_normal_(model.weight, gain=nn.init.calculate_gain('relu'))
+        nn.init.xavier_normal_(model.weight, gain=nn.init.calculate_gain("relu"))
         if model.bias is not None:
             nn.init.zeros_(model.bias)
-        
+
     elif isinstance(model, nn.Linear):
         nn.init.xavier_normal_(model.weight)
         if model.bias is not None:
             nn.init.zeros_(model.bias)
-            
+
+
 def print_while_trainig(epochs_list, loss_history, loss_history_overclustering):
     """Prints loss and overcluster loss
-    
+
     Parameters
     ----------
     epochs_list : list of ints
@@ -57,25 +58,27 @@ def print_while_trainig(epochs_list, loss_history, loss_history_overclustering):
     loss_history_overclustering : list of floats
         overcluseting losses, the same size as the epochs_list
     """
-    
-    clear_output(True)
-    
-    fig, (ax1, ax2) = plt.subplots(1, 2, constrained_layout=True)
-    fig.set_figwidth(12)     
 
-    ax1.plot(epochs_list, loss_history, label='train_loss')
+    clear_output(True)
+
+    fig, (ax1, ax2) = plt.subplots(1, 2, constrained_layout=True)
+    fig.set_figwidth(12)
+
+    ax1.plot(epochs_list, loss_history, label="train_loss")
     ax1.legend()
     ax1.grid()
 
-    ax2.plot(epochs_list, loss_history_overclustering, label='train_loss_overclustering')
+    ax2.plot(
+        epochs_list, loss_history_overclustering, label="train_loss_overclustering"
+    )
     ax2.legend()
     ax2.grid()
     plt.show()
-    
-    
+
+
 def get_cluster_labeling(model, dataloader, device=torch.device("cpu")):
     """Assing the cluster labels for the data from dataloader
-    
+
     Parameters
     ----------
     model : resnetModel
@@ -84,32 +87,31 @@ def get_cluster_labeling(model, dataloader, device=torch.device("cpu")):
         the dataloader with the data to cluster
     device : torch.device
         device to perform the clustering on
-        
+
     Returns
     ----------
-    answers : 
+    answers :
         original_labels : list of int
             original labels from the dataset
         cluster_labels :  list of int
             labels obtained from the model
     """
     model.eval()
-    original_labels= []
-    cluster_labels = [] 
+    original_labels = []
+    cluster_labels = []
     for batch in dataloader:
         images = batch["original"].to(device)
         labels = batch["label"].to(device)
         outputs = model(images, False)
         original_labels += labels.tolist()
-        cluster_labels += torch.argmax(outputs, dim = 1).tolist()
+        cluster_labels += torch.argmax(outputs, dim=1).tolist()
     return original_labels, cluster_labels
-    
-    
-    
+
+
 def visualise_clusetering_results(original_labels, cluster_labels, figwidth=20):
     """Visualises clusterisation results
     Shows, how the images with certain original labels are distributed fater clusterization
-    
+
     Parameters:
     ----------
     original_labels : list of int
@@ -119,13 +121,12 @@ def visualise_clusetering_results(original_labels, cluster_labels, figwidth=20):
     figwidth : int, optional
         width of plt figure
     """
-    
+
     original_labels = np.array(original_labels)
     cluster_labels = np.array(cluster_labels)
     class_ids = np.unique(original_labels)
     fig, axes = plt.subplots(2, 5, constrained_layout=True)
     fig.set_figwidth(figwidth)
-
 
     for idx, ax in enumerate(axes.reshape(-1)):
         labels_distribution = original_labels[cluster_labels == idx]
@@ -133,15 +134,15 @@ def visualise_clusetering_results(original_labels, cluster_labels, figwidth=20):
         ax.bar(list(range(10)), counts)
         ax.set_xticks(np.arange(10))
         ax.set_xlim([0, 9])
-        ax.set_title("Original label: %i"%idx)
-        
-        
+        ax.set_title("Original label: %i" % idx)
+
+
 class startifiedAgentDataset(Dataset):
     """
-    The dataset produced by stratified_split function. 
+    The dataset produced by stratified_split function.
     This datset takes the data directly from the initial dataset instance.
-    It can acces only a certain part of original dataset data 
-    
+    It can acces only a certain part of original dataset data
+
     Attributes
     ----------
     original_dataset : torch.utils.data.dataset.Dataset like
@@ -149,25 +150,25 @@ class startifiedAgentDataset(Dataset):
     indices_list : list
         list of indices wich the can be acessed from the orgignal dataset
     """
-    
+
     def __init__(self, original_dataset, indices_list):
         super().__init__()
         self.original_dataset = original_dataset
         self.indices_list = indices_list
-        
+
     def __len__(self):
         return len(self.indices_list)
-    
+
     def __getitem__(self, idx):
         original_idx = self.indices_list[idx]
         return self.original_dataset[original_idx]
-    
-    
+
+
 def stratified_split(original_dataset, train_size, label_key="label"):
     """Split the generic torch dataset in a stratified manned
     Based on sklearn.model_selection.train_test_split function
-    The output datasets contain no data but refer to a certan indices in original dataset. 
-    
+    The output datasets contain no data but refer to a certan indices in original dataset.
+
     Parameters
     ----------
     original_dataset : torch.utils.data.dataset.Dataset like
@@ -175,29 +176,31 @@ def stratified_split(original_dataset, train_size, label_key="label"):
     train_size : float
         a fraction of the data train (first returned argument) dataset
     label_key : str or int
-        a key to access the label from the original dataset. 
+        a key to access the label from the original dataset.
         If original dataset returns a dict, it should prabably be "label"
         If original dataset returns a tuble, it should be int and in most cases equal to 1
-    
+
     Returns
     ----------
     train_dataset : startifiedAgentDataset
         train part of the split. The realtive size is defined by train_size parameter
     test_dataset : startifiedAgentDataset
         test part of the split. The realtive size is len(original_dataset) - train_size
-    
-        
+
+
     """
-    
+
     labels = [x[label_key] for x in original_dataset]
     indices = np.arange(len(labels))
     indices_and_labels = np.vstack([indices, labels]).transpose()
-    train_indices_and_labels, test_indices_and_labels = train_test_split(indices_and_labels, train_size=train_size, stratify=labels)
-    train_indices = train_indices_and_labels[:,0].astype(np.int)
-    test_indices = test_indices_and_labels[:,0].astype(np.int)
+    train_indices_and_labels, test_indices_and_labels = train_test_split(
+        indices_and_labels, train_size=train_size, stratify=labels
+    )
+    train_indices = train_indices_and_labels[:, 0].astype(np.int)
+    test_indices = test_indices_and_labels[:, 0].astype(np.int)
     train_dataset = startifiedAgentDataset(original_dataset, train_indices)
     test_dataset = startifiedAgentDataset(original_dataset, test_indices)
-    
+
     return train_dataset, test_dataset
 
 
@@ -211,17 +214,17 @@ def create_mapping(original_labels, cluster_labels):
         true labels of the data
     cluster_labels : list of int
         labels produced by a clustering algotithm
-    
+
     Returns
     -------
     mapping : dict
         mapping from the clusters to original classes
-        
+
     Notes
     -----
-    This function should be used only if the clusters are well-defined    
+    This function should be used only if the clusters are well-defined
     """
-    
+
     original_labels = np.array(original_labels, dtype=np.int)
     cluster_labels - np.array(cluster_labels, dtype=np.int)
     class_ids = np.unique(original_labels)
@@ -231,31 +234,33 @@ def create_mapping(original_labels, cluster_labels):
         original_labels_in_cluster = original_labels[cluster_labels == cluster_id]
         map_to_id = np.bincount(original_labels_in_cluster).argmax()
         mapping[cluster_id] = map_to_id
-        
+
     return mapping
 
+
 def print_mapping(mapping):
-    '''Visualises the mapping
-    
+    """Visualises the mapping
+
     Parameters
     ----------
     mapping : dict (int to int)
         a mapping from clusters to classes
-    '''
+    """
     print("Cluster  Class")
     for key, value in mapping.items():
-        print("%3i -----> %i"%(key, value))
+        print("%3i -----> %i" % (key, value))
+
 
 class LinearClusetrizator(nn.Module):
-    '''linear clusterisator for IIC. Useful for testing purposes'''
-    
+    """linear clusterisator for IIC. Useful for testing purposes"""
+
     def __init__(self):
         super(LinearClusetrizator, self).__init__()
-        
+
         self.linear = nn.Linear(28 * 28, 10)
         self.flatten = nn.Flatten()
         self.softmax = nn.Softmax(dim=1)
-        
+
     def forward(self, x, overclustering=False):
         x = self.flatten(x)
         x = self.linear(x)
